ac414a54f307ea59a110f24b93c3841be7a7a848
Matt Krueger <mkrueger@rstms.net>
2026-01-02 22:36:12 -0700

    Bump version: 0.0.1 → 0.0.2

--- cmd/root.go
+++ cmd/root.go
@@ -41,7 +41,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.1",
+	Version: "0.0.2",
 	Use:     "smtpd-filter-addheader HEADER [HEADER...]",
 	Short:   "smtpd filter for adding static email header lines",
 	Long: `
